471a7a1c88e28fbb955d894c1a0bda471a2f893b
farkon00 <sammer2016sammer@gmail.com>
2022-05-09 21:53:40 +0300

    First test commands added

---
+++ commands.py
@@ -0,0 +1,33 @@
+import zlib
+import os
+import datetime
+
+def last_obj():
+    with open(".git/logs/refs/heads/main", "r") as f:
+        log = f.read()[:-1]
+
+    last_log = log[log.rfind("\n")+1:]
+    info = last_log.split()
+    print(
+f"""
+Hash: {info[1]}
+Author: {info[2]} {info[3]}
+Date: {datetime.datetime.fromtimestamp(int(info[4]))} {info[5]}
+{" ".join(info[6:])}
+"""
+    )
+
+def log_actions(num: int):
+    with open(".git/logs/refs/heads/main", "r") as f:
+        log = f.read()[:-1]
+
+    for i in log.split("\n")[::-1][:num][::-1]:
+        info = i.split()
+        print(
+f"""
+Hash: {info[1]}
+Author: {info[2]} {info[3]}
+Date: {datetime.datetime.fromtimestamp(int(info[4]))} {info[5]}
+{" ".join(info[6:])}
+"""
+        )
---
+++ pagit.py
@@ -0,0 +1,18 @@
+import sys
+
+from commands import *
+
+""" Code for later
+for i in os.listdir(".git/objects"):
+    if i not in ("pack", "info"):
+        for j in os.listdir(f".git/objects/{i}"):
+            print(zlib.decompress(open(f".git/objects/{i}/{j}", "rb").read()))"""
+
+if len(sys.argv) < 2:
+    print("Usage: python pagit.py <command>")
+else:
+    match sys.argv[1]:
+        case "last-obj":
+            last_obj()
+        case "log":
+            log_actions(int(sys.argv[2]) if len(sys.argv) > 2 else 2)
