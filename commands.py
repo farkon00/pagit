fdf09690a284629c38f8ac9adf25162acf10b75b
farkon00 <sammer2016sammer@gmail.com>
2022-05-09 21:57:11 +0300

    Fixed slice

--- commands.py
+++ commands.py
@@ -19,9 +19,9 @@ Date: {datetime.datetime.fromtimestamp(int(info[4]))} {info[5]}
 
 def log_actions(num: int):
     with open(".git/logs/refs/heads/main", "r") as f:
-        log = f.read()[:-1]
+        log = f.read()[:-1].split("\n")
 
-    for i in log.split("\n")[::-1][:num][::-1]:
+    for i in log[len(log)-num:]:
         info = i.split()
         print(
 f"""
